bee1a70e8926e711548d3f158868f45fb7fc5db0
Diepvu <150464946+diepphivu@users.noreply.github.com>
2024-06-27 15:20:35 +0700

    change ipynb and qgis

--- lib/find_stations.py
+++ lib/find_stations.py
@@ -2,7 +2,8 @@ import pandas as pd
 import geopandas
 import pulp
 import os
-from spopt.locate import LSCP
+import spopt
+from spopt.locate import LSCP, PCenter
 from lib.utils import network_distance, facility_points_calulator
 
 
@@ -25,7 +26,7 @@ def find_stations_LSCP(req):
     SERVICE_RADIUS = round(SERVICE_RADIUS, 2)
 
     lscp = LSCP.from_cost_matrix(cost_matrix, SERVICE_RADIUS)
-    lscp = lscp.solve(pulp.GLPK(msg=False))
+    lscp = lscp.solve(pulp.CPLEX_CMD(msg=False))
 
     lscp_objval = lscp.problem.objective.value()
 
@@ -46,44 +47,67 @@ def find_stations_PCenter(req):
     facility_points = facility_points_calulator(network)
 
     pivot_table = network.pivot_table(
-        values="Distance", index="EndPoint", columns="StartPoint", fill_value=0
+        values="Distance", index="EndPoint", columns="StartPoint"
     )
-    cost_matrix = pivot_table.astype(int)
+    cost_matrix = pivot_table.fillna(0).astype(int)
 
-    num_points = cost_matrix.shape[0]
-    model = pulp.LpProblem("p-Center Problem", pulp.LpMinimize)
+    # num_points = cost_matrix.shape[0]
+    # model = pulp.LpProblem("p-Center Problem", pulp.LpMinimize)
 
-    x = pulp.LpVariable.dicts(
-        "x", (range(num_points), range(num_points)), 0, 1, pulp.LpBinary
+    # x = pulp.LpVariable.dicts(
+    #     "x", (range(num_points), range(num_points)), 0, 1, pulp.LpBinary
+    # )
+    # y = pulp.LpVariable.dicts("y", range(num_points), 0, 1, pulp.LpBinary)
+    # z = pulp.LpVariable("z", 0)
+    # model += z
+
+    # for i in range(num_points):
+    #     model += pulp.lpSum(x[i][j] for j in range(num_points)) == 1
+
+    # model += pulp.lpSum(y[j] for j in range(num_points)) == p_facilities
+
+    # for i in range(num_points):
+    #     for j in range(num_points):
+    #         model += x[i][j] <= y[j]
+    #         model += z >= cost_matrix.iloc[i, j] * x[i][j]
+
+    # model.solve()
+
+    # selected_facilities = []
+    # for j in range(num_points):
+    #     if pulp.value(y[j]) == 1:
+    #         facility_info = facility_points.iloc[j]
+    #         selected_facilities.append(
+    #             {
+    #                 "Id": facility_info["Id"],
+    #                 "FacilityPoints": facility_info["FacilityPoints"],
+    #                 "XX": facility_info["XX"],
+    #                 "YY": facility_info["YY"],
+    #             }
+    #         )
+
+    # selected_facilities_df = pd.DataFrame(selected_facilities)
+
+    # # Tạo bảng pivot từ dataframe
+    # pivot_table = network.pivot_table(
+    #     values="Distance", index="EndPoint", columns="StartPoint", fill_value=0
+    # )
+
+    # # Chuyển đổi thành ma trận numpy để sử dụng với spopt và pulp
+    # cost_matrix = pivot_table.values.astype(int)
+
+    # Khởi tạo và giải quyết bài toán LSCP
+    pcenter = PCenter.from_cost_matrix(cost_matrix, p_facilities)
+    pcenter = pcenter.solve(pulp.CPLEX_CMD(msg=False))
+
+    # Lấy giá trị objective
+    pcenter_objval = pcenter.problem.objective.value()
+
+    # Tạo danh sách các điểm được chọn trong pcenter_objval
+    selected_facilities = [i for i, dv in enumerate(pcenter.fac_vars) if dv.varValue]
+
+    # Tạo DataFrame từ danh sách các điểm được chọn
+    selected_facilities_df = facility_points.iloc[selected_facilities].reset_index(
+        drop=True
     )
-    y = pulp.LpVariable.dicts("y", range(num_points), 0, 1, pulp.LpBinary)
-    z = pulp.LpVariable("z", 0)
-    model += z
-
-    for i in range(num_points):
-        model += pulp.lpSum(x[i][j] for j in range(num_points)) == 1
-
-    model += pulp.lpSum(y[j] for j in range(num_points)) == p_facilities
-
-    for i in range(num_points):
-        for j in range(num_points):
-            model += x[i][j] <= y[j]
-            model += z >= cost_matrix.iloc[i, j] * x[i][j]
-
-    model.solve()
-
-    selected_facilities = []
-    for j in range(num_points):
-        if pulp.value(y[j]) == 1:
-            facility_info = facility_points.iloc[j]
-            selected_facilities.append(
-                {
-                    "Id": facility_info["Id"],
-                    "FacilityPoints": facility_info["FacilityPoints"],
-                    "XX": facility_info["XX"],
-                    "YY": facility_info["YY"],
-                }
-            )
-
-    selected_facilities_df = pd.DataFrame(selected_facilities)
     return selected_facilities_df.to_dict(orient="records")
